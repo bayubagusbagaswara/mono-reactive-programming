95bf738c218e22510bcff73f8b24987261d4eeda
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2025-05-18 10:35:56 +0700

    init

---
+++ src/main/java/com/bayu/mono_reactive_programming/MonoReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.mono_reactive_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoReactiveProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/mono_reactive_programming/MonoReactiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.mono_reactive_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoReactiveProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
